fb08cefeee059bd957c4b5d899b4d9380c7fda38
Ronaldo jose <ronaldo.jrk@gmail.com>
2021-03-27 00:04:46 -0300

    🎉🤗

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import { getPrismicClient } from '../services/prismic';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
+import { FiCalendar, FiUser } from "react-icons/fi";
+
 interface Post {
   uid?: string;
   first_publication_date: string | null;
@@ -24,9 +26,37 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+
+  return (
+
+    <>
+      <main className={styles.container}>
+        <img src="/images/logo.svg" alt="logo" />
+        <div className={styles.posts}>
+          <a href="">
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <time>
+                <span><FiCalendar /></span>
+                15 Mar 2021
+              </time>
+
+              <p>
+                <span><FiUser /></span>
+                Joseph Oliveira
+              </p>
+            </div>
+          </a>
+
+        </div>
+        <button type="button">Carregar mais posts</button>
+      </main>
+    </>
+  );
+  // TODO
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
